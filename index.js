c67384ec4235875d19fb05dc217835681fa63a15
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-12-01 14:38:35 +0600

    learn server api creation

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('hello this is my first node project')
-  })
+    res.send('hello this is my first node project');
+  });
+
+app.get('/Profile', (req, res) =>{
+  res.send({name: 'Nura Alam', Age: 24, Home: 'Cumilla, Bangladesh'})
+});
   
 app.listen(3000, ()=>console.log('App is running'));
