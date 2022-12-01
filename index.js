6741c0bd646ac7f5c5345419284bff68db712799
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-12-01 19:55:53 +0600

    data fetch from backend server

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 
 const express = require('express');
+var cors = require('cors');
 const app = express();
+
+app.use(cors());
+
+const users = ['Abir', 'Soham', 'Faruk', 'Robin', 'Tutul'];
 
 app.get('/', function (req, res) {
     res.send('hello this is my first node project');
   });
 
-app.get('/Profile', (req, res) =>{
-  res.send({name: 'Nura Alam', Age: 24, Home: 'Cumilla, Bangladesh'})
+app.get('/users/:id', (req, res) =>{
+  const id = req.params.id;
+  const name = users[id];
+  res.send({id, name});
 });
   
 app.listen(3000, ()=>console.log('App is running'));
